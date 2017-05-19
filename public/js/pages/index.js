85b459a7c5ab92461c4b75d5d0c32ddb327e6f09
Martin Shaw <15090190@stu.mmu.ac.uk>
2017-05-19 12:28:17 +0100

    Finished splashscreen. Starting changes to Nav Bar

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -361,15 +361,39 @@ module.exports = __webpack_require__(8);
 var tilt = __webpack_require__(33);
 // const recognizeSpeech = require('recognize-speech') ;
 
+var splashscreen = false;
 
 $(function () {
 
-	// Load random splashscreen image from array
-	var splashscreenPath = "/images/splashscreen_bg/";
-	var splashscreenOptions = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-	var splashscreenStyleTag = "<style>.splashscreen_container::after{content: '';background-image: url(" + splashscreenPath + splashscreenOptions[Math.floor(Math.random() * splashscreenOptions.length)] + ");background-size: cover;width: 100%; height: 100%;opacity: 0.12;top: 0;left: 0;bottom: 0;right: 0;position: absolute;z-index: 3;}</style>";
+	// If debugging, skip splashscreen
+	if (splashscreen != true) {
+		$(".splashscreen_container").css("opacity", 0);
+		$(".splashscreen_container").hide();
+	} else {
+		// Load random splashscreen image from array
+		var splashscreenPath = "/images/splashscreen_bg/";
+		var splashscreenOptions = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+		var splashscreenStyleTag = "<style>.splashscreen_container::after{content: '';background-image: url(" + splashscreenPath + splashscreenOptions[Math.floor(Math.random() * splashscreenOptions.length)] + ");background-size: cover;width: 100%; height: 100%;opacity: 0.12;top: 0;left: 0;bottom: 0;right: 0;position: absolute;z-index: 3;}</style>";
 
-	$("body").append(splashscreenStyleTag);
+		$("body").append(splashscreenStyleTag);
+
+		// Hide splashscreen after display
+		setTimeout(function () {
+			$(".splashscreen_container").animate({
+				opacity: 0
+			}, 1000, function () {
+				alert();
+			});
+		}, 7500);
+	}
+
+	// Add event handling for Showcase Items
+	$(".s_m_item").hover(function () {
+		$(this).find(".s_m_i_container").animate({ opacity: 1 }, 400);
+	});
+	$(".s_m_item").mouseleave(function () {
+		$(this).find(".s_m_i_container").animate({ opacity: 0 }, 100);
+	});
 
 	// $(".s_m_item").tilt({
 	// 	perspective: 1300
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,10 @@
 @section('content')
 	<div class="splashscreen_container">
 		<div class="splashscreen">
-			<div class="ss_logo"><a href="/">Martin Shaw</a></div>
+			<div class="ss_logo">
+				@component('components.logo')
+				@endcomponent
+			</div>
 			<div class="ss_tagline">
 				Experienced Software Engineer &amp; Web Developer building complex client projects of all types &amp; sizes.
 				<b>I'm currently seeking a 12-month Industrial Placement</b>
@@ -28,7 +31,7 @@
 		</ul>
 		<div class="mainnav_title">
 			<a href="/" class="mn_t_linktohomepage">
-				Martin Shaw
+				<img src="images/logo/logo_normal.png" alt="">
 			</a>
 		</div>
 	</div>
